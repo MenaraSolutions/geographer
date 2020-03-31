0e776a089c7560ce1920b0ee1a9c960da318e8a4
Denis Mysenko <denis@mysenko.com>
2020-03-31 17:42:51 +1100

    Remove unnecessary method (there is a magic method already)

--- src/State.php
+++ src/State.php
@@ -31,7 +31,7 @@ class State extends Divisible
         'fipsCode' => 'ids.fips',
         'isoCode' => 'ids.iso_3166_2',
         'geonamesCode' => 'ids.geonames',
-        'zipRanges' => 'zip_ranges',
+        'postCodes' => 'postcodes',
         'name'
     ];
 
@@ -41,19 +41,5 @@ class State extends Divisible
     public function getCities()
     {
         return $this->getMembers();
-    }
-
-    /**
-     * @return array
-     */
-    public function getZipRanges()
-    {
-        $ranges = $this->meta['zip_ranges'];
-
-        if ($zip_ranges === null) {
-            return [];
-        }
-
-        return $ranges;
     }
 }
